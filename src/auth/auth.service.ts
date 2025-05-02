bbf161e658257a91dbe88c3721716f80fbc61612
KienLee <Kien.LT225023@sis.hust.edu.vn>
2025-05-02 15:49:11 +0700

    feat: add login and register

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,51 @@
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from '../db/prisma.service';
+import * as bcrypt from 'bcryptjs';
+import { LoginDto } from './dto/login.dto';
+import { RegisterDto } from './dto/register.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(private prisma: PrismaService, private jwt: JwtService) {}
+
+  async register(dto: RegisterDto) {
+    const userExists = await this.prisma.user.findUnique({
+      where: { email: dto.email },
+    });
+    if (userExists) throw new BadRequestException('Email đã được sử dụng');
+
+    const hashed = await bcrypt.hash(dto.password, 10);
+    const user = await this.prisma.user.create({
+      data: {
+        username: dto.username,
+        displayName: dto.displayName,
+        status: 'Active',
+        role: 'User',
+        email: dto.email,
+        password: hashed,
+      },
+    });
+
+    return {
+      message: 'Đăng ký thành công',
+      user: { id: user.id, username: user.username, email: user.email },
+    };
+  }
+
+  async login(dto: LoginDto) {
+    const user = await this.prisma.user.findFirst({
+      where: {
+        OR: [{ email: dto.identifier }, { username: dto.identifier }],
+      },
+    });
+
+    if (!user) throw new BadRequestException('Người dùng không tồn tại');
+
+    const isMatch = await bcrypt.compare(dto.password, user.password);
+    if (!isMatch) throw new BadRequestException('Sai mật khẩu');
+
+    const token = this.jwt.sign({ sub: user.id });
+    return { token, user: { id: user.id, username: user.username, email: user.email } };
+  }
+}
